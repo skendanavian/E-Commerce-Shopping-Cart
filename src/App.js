929681e726292cfe577001a81ffd98e898e742d4
skendanavian <s.nissen.bass@gmail.com>
2021-01-16 16:31:56 -0600

    added product data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="grid-container">
-      <header href="/">Online Store</header>
+      <header>
+        <a href="/">Boarder World</a>
+      </header>
       <main>Product List</main>
       <footer>All Rights Reserved - 2020 </footer>
     </div>
