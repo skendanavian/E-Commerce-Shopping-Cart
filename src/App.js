17676ba30c856bdde362ef6ac99661349ce9aecd
skendanavian <s.nissen.bass@gmail.com>
2021-01-16 12:47:29 -0600

    setup initial project and page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <header href="/">Online Store</header>
+      <main>Product List</main>
+      <footer>All Rights Reserved - 2020 </footer>
     </div>
   );
 }
