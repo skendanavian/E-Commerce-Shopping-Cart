68433dfc67038409a4ef08c5d301984349979bfc
skendanavian <s.nissen.bass@gmail.com>
2021-01-17 15:51:49 -0600

    size filter working

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ class App extends React.Component {
       sort: "",
     };
   }
-  sortProducts(event) {
+  sortProducts = (event) => {
     console.log(event.target.value);
-  }
-  filterProducts(event) {
-    console.log(event.target.value);
-  }
+  };
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) > 0
+        ),
+      });
+    }
+  };
 
   render() {
     return (
