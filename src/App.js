832a962878734f532a0316a42f3d58cbf4b0f8a7
skendanavian <s.nissen.bass@gmail.com>
2021-01-17 11:29:04 -0600

    updated filter component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
 
@@ -11,15 +12,29 @@ class App extends React.Component {
       sort: "",
     };
   }
+  sortProducts(event) {
+    console.log(event.target.value);
+  }
+  filterProducts(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="grid-container">
         <header>
-          <a href="/">Boarder World</a>
+          <a href="/">The Snow Shack</a>
         </header>
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">Cart Items</div>
