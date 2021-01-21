70628ef4c667f21abbe17951cae86818cf458a01
skendanavian <s.nissen.bass@gmail.com>
2021-01-21 15:39:08 -0600

    add auth0 admin authentication button and form

--- src/App.js
+++ src/App.js
@@ -5,29 +5,39 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import AdminScreen from "./screens/AdminScreen";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import AuthenticationButton from "./components/AuthenticationButton";
+import LoginButton from "./components/LoginButton";
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <div className="grid-container">
-            <header>
-              <Link to="/">
-                <img src={mountain} alt="mountain"></img> React Mountain Sports
-              </Link>
-            </header>
-            <main>
-              <Route path="/admin" component={AdminScreen} />
-              <Route path="/" component={HomeScreen} exact />
-            </main>
-            <footer>
-              <div>
-                <Link to="/admin">Admin Login</Link>
-              </div>
-              <div> All Rights Reserved - 2020</div>
-            </footer>
-          </div>
+          <Auth0ProviderWithHistory>
+            <div className="grid-container">
+              <header>
+                <Link to="/">
+                  <img src={mountain} alt="mountain"></img> React Mountain
+                  Sports
+                </Link>
+              </header>
+              <main>
+                <Route path="/admin" component={AdminScreen} />
+
+                <Route path="/" component={HomeScreen} exact />
+              </main>
+              <footer>
+                <div>
+                  {/* <AuthenticationButton> */}
+                  <Link to="/admin">Admin </Link>
+                  <LoginButton></LoginButton>
+                  {/* </AuthenticationButton> */}
+                </div>
+                <div> All Rights Reserved - 2020</div>
+              </footer>
+            </div>
+          </Auth0ProviderWithHistory>
         </BrowserRouter>
       </Provider>
     );
