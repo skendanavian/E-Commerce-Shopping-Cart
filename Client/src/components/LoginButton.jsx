7cfb475050d17d4890433664250ea91ceb3eff5b
skendanavian <s.nissen.bass@gmail.com>
2021-01-22 10:25:01 -0600

    admin authentication working

--- Client/src/components/LoginButton.jsx
+++ Client/src/components/LoginButton.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
-  return <button onClick={() => loginWithRedirect()}>Admin Login</button>;
+  return (
+    <button onClick={() => loginWithRedirect("http://localhost:3000/admin")}>
+      <Link to="/admin">Admin Login</Link>
+    </button>
+  );
 };
 
 export default LoginButton;
