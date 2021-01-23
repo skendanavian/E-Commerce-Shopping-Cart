813beb5e1c051340111ba48be376dc0d2ef17b51
skendanavian <s.nissen.bass@gmail.com>
2021-01-22 20:36:45 -0600

    server fix

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/build"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+app.get("/admin", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
